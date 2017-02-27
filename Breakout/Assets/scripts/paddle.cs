5ac033c553b9209a168a8dc0a2b4a6fb094574b0
corri.moran <moranc3@hawkmail.newpaltz.edu>
2017-02-27 10:41:50 -0500

    Added unity files

---
+++ Breakout/Assets/scripts/paddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddle : MonoBehaviour {
+
+	Rigidbody2D rigidBody;
+	float speed = 5;
+
+	void Start () {
+		rigidBody = GetComponent <Rigidbody2D> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxisRaw ("Horizontal");
+		x = x * speed; 
+		rigidBody.velocity = new Vector2 (x, 0);
+	}
+
+}
